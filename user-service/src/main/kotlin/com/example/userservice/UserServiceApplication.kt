e0d47e7a88257af5df918cdca45ed4b921277cfa
shane <psh40963@naver.com>
2022-11-05 16:42:08 +0900

    User service created

---
+++ user-service/src/main/kotlin/com/example/userservice/UserServiceApplication.kt
@@ -0,0 +1,14 @@
+package com.example.userservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+
+@SpringBootApplication
+@EnableDiscoveryClient
+class UserServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<UserServiceApplication>(*args)
+}
