d13575ce84188edc95cef3ab717f6b12883491b1
shane <psh40963@naver.com>
2022-11-09 21:37:07 +0900

    Zuul Logging Filter

---
+++ zuul-service/src/main/java/com/example/zuulservice/filter/ZuulLoggingFilter.java
@@ -0,0 +1,40 @@
+package com.example.zuulservice.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+@Slf4j
+public class ZuulLoggingFilter extends ZuulFilter {
+
+    @Override
+    public Object run() throws ZuulException {
+        log.info("********** printing logs ** : Started");
+
+        HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+        log.info("request uri -> {}", request.getRequestURI());
+        log.info("********** printing logs ** : Ended\n");
+        return null;
+    }
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+}
