063ac8cf2642a6f54607d9c1a42f2a7a59678404
shane <psh40963@naver.com>
2022-11-18 21:38:28 +0900

    catalog-service : init

---
+++ catalog-service/src/main/kotlin/com/example/catalogservice/CatalogServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.catalogservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CatalogServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<CatalogServiceApplication>(*args)
+}
