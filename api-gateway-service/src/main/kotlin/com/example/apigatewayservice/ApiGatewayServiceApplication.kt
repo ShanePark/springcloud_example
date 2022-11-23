544d3675b3ab6de823660de12019c69f03e0cde2
shane <psh40963@naver.com>
2022-11-23 20:54:02 +0900

    feat: Gateway-service actuator

--- api-gateway-service/src/main/kotlin/com/example/apigatewayservice/ApiGatewayServiceApplication.kt
+++ api-gateway-service/src/main/kotlin/com/example/apigatewayservice/ApiGatewayServiceApplication.kt
@@ -1,11 +1,19 @@
 package com.example.apigatewayservice
 
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
 class ApiGatewayServiceApplication
 
 fun main(args: Array<String>) {
     runApplication<ApiGatewayServiceApplication>(*args)
+}
+
+@Bean
+fun httpTraceRepository(): HttpTraceRepository {
+    return InMemoryHttpTraceRepository()
 }
