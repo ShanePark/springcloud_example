94abbcecd47f1d3aa28e0322ebc109d4a9817ddb
shane <psh40963@naver.com>
2022-12-04 17:17:41 +0900

    mariaDB for Kafka

--- user-service/src/main/kotlin/com/example/userservice/client/OrderServiceClient.kt
+++ user-service/src/main/kotlin/com/example/userservice/client/OrderServiceClient.kt
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable
 @FeignClient("order-service")
 interface OrderServiceClient {
 
-    @GetMapping("/order-service/{userId}/orders_wrong")
+    @GetMapping("/order-service/{userId}/orders")
     fun getOrders(@PathVariable userId: String): List<ResponseOrder>
 
 }
